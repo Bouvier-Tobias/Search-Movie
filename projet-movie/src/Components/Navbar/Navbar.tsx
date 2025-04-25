1afa492f49a140992b0e1aa55477773428758425
Tobias <bouviertobiaspro@gmail.com>
2025-04-25 03:36:36 +0200

    Add: Ajoue du projet React

---
+++ projet-movie/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Accueil</li>
+        <li>Catalogue</li>
+        <li>A Propos</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ projet-movie/src/Pages/Catalogue/Catalogue.tsx
@@ -0,0 +1,5 @@
+const Catalogue = () => {
+  return <h1>Catalogue</h1>;
+};
+
+export default Catalogue;
